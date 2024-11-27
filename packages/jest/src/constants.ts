82863915d0fada33ef6995fe26ac5434d50222ba
Leo Hentschker <leowork@Leos-MBP.lan>
2024-11-26 19:22:28 -0500

    Removing even more unused items

--- packages/jest/src/constants.ts
+++ packages/jest/src/constants.ts
@@ -1,4 +1,3 @@
-export const DEFAULT_DESIGN_SNAPSHOT_DIR = "__tests__/__design_snapshots__";
 export const DEFAULT_REVIEW_ENDPOINT =
   "https://vslint-644118703752.us-central1.run.app/api/v1/design-review";
 export const DEFAULT_LOCAL_REVIEW_ENDPOINT =
