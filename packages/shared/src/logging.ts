e262e9a8fb46b07aa4f02a1a57141600abe3b8e6
Leo Hentschker <leowork@Leos-MacBook-Pro.local>
2024-12-01 11:12:31 -0500

    Adding in vitest support

---
+++ packages/shared/src/logging.ts
@@ -0,0 +1,22 @@
+import winston from "winston";
+import type { DesignReviewRun } from "./render";
+
+let _LOGGER: null | winston.Logger = null;
+
+export const getLogger = (logInput?: DesignReviewRun["log"]) => {
+  if (!logInput || typeof logInput === "string") {
+    if (!_LOGGER) {
+      _LOGGER = winston.createLogger({
+        level: logInput || "info",
+        transports: [new winston.transports.Console()],
+      });
+    }
+    return _LOGGER;
+  }
+  return logInput;
+};
+
+export const setLogLevel = (level: "info" | "debug") => {
+  const logger = getLogger();
+  logger.level = level;
+};
---
+++ packages/vitest/src/index.ts
@@ -0,0 +1,13 @@
+import { DEFAULT_RULES } from "@vslint/shared";
+import {
+  DEFAULT_REVIEW_ENDPOINT,
+  DEFAULT_REVIEW_TIMEOUT,
+} from "@vslint/shared";
+import { extendExpectDesignReviewer } from "./extend-expect";
+
+export {
+  extendExpectDesignReviewer,
+  DEFAULT_REVIEW_ENDPOINT,
+  DEFAULT_REVIEW_TIMEOUT,
+  DEFAULT_RULES,
+};
