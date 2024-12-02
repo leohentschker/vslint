cdc22b86c1a272d55bc69a5b587ed2ff35a85f50
Leo Hentschker <lhentschker@gmail.com>
2024-12-02 10:42:21 -0500

    test(packages/shared/src/tests.ts): fail in CI when snapshots do not match (#70)
    Co-authored-by: Leo Hentschker <leowork@Leos-MBP.lan>

--- packages/shared/src/tests.test.ts
+++ packages/shared/src/tests.test.ts
@@ -10,16 +10,18 @@ test("extendExpectDesignReviewer passes with cached passing snapshot", async () 
       customStyles: [],
       model: { modelName: "gpt-4o-mini" },
     },
-    () => ({
-      snapshotPath: "snapshot-path",
-      snapshotIdentifier: "snapshot-identifier",
-      markSnapshotAsReviewed: vitest.fn(),
-      existingSnapshot: {
-        contentHash: getContentHash(sampleElement.outerHTML),
-        failedRules: [],
-        pass: true,
-      },
-    }),
+    {
+      getSnapshotData: () => ({
+        snapshotPath: "snapshot-path",
+        snapshotIdentifier: "snapshot-identifier",
+        markSnapshotAsReviewed: vitest.fn(),
+        existingSnapshot: {
+          contentHash: getContentHash(sampleElement.outerHTML),
+          failedRules: [],
+          pass: true,
+        },
+      }),
+    },
   );
   const result = await matcher.toPassDesignReview(sampleElement);
   expect(result.pass).toBe(true);
@@ -33,16 +35,18 @@ test("extendExpectDesignReviewer fails with cached failing snapshot", async () =
       customStyles: [],
       model: { modelName: "gpt-4o-mini" },
     },
-    () => ({
-      snapshotPath: "snapshot-path",
-      snapshotIdentifier: "snapshot-identifier",
-      markSnapshotAsReviewed: vitest.fn(),
-      existingSnapshot: {
-        contentHash: getContentHash(sampleElement.outerHTML),
-        failedRules: ["rule-1"],
-        pass: false,
-      },
-    }),
+    {
+      getSnapshotData: () => ({
+        snapshotPath: "snapshot-path",
+        snapshotIdentifier: "snapshot-identifier",
+        markSnapshotAsReviewed: vitest.fn(),
+        existingSnapshot: {
+          contentHash: getContentHash(sampleElement.outerHTML),
+          failedRules: ["rule-1"],
+          pass: false,
+        },
+      }),
+    },
   );
   const result = await matcher.toPassDesignReview(sampleElement);
   expect(result.pass).toBe(false);
@@ -57,16 +61,18 @@ test("extendExpectDesignReviewer passes with cached failing snapshot but not str
       model: { modelName: "gpt-4o-mini" },
       strict: false,
     },
-    () => ({
-      snapshotPath: "snapshot-path",
-      snapshotIdentifier: "snapshot-identifier",
-      markSnapshotAsReviewed: vitest.fn(),
-      existingSnapshot: {
-        contentHash: getContentHash(sampleElement.outerHTML),
-        failedRules: ["rule-1"],
-        pass: false,
-      },
-    }),
+    {
+      getSnapshotData: () => ({
+        snapshotPath: "snapshot-path",
+        snapshotIdentifier: "snapshot-identifier",
+        markSnapshotAsReviewed: vitest.fn(),
+        existingSnapshot: {
+          contentHash: getContentHash(sampleElement.outerHTML),
+          failedRules: ["rule-1"],
+          pass: false,
+        },
+      }),
+    },
   );
   const result = await matcher.toPassDesignReview(sampleElement);
   expect(result.pass).toBe(true);
@@ -80,19 +86,67 @@ test("extendExpectDesignReviewer passes with cached failing snapshot but not str
       customStyles: [],
       model: { modelName: "gpt-4o-mini" },
     },
-    () => ({
-      snapshotPath: "snapshot-path",
-      snapshotIdentifier: "snapshot-identifier",
-      markSnapshotAsReviewed: vitest.fn(),
-      existingSnapshot: {
-        contentHash: getContentHash(sampleElement.outerHTML),
-        failedRules: ["rule-1"],
-        pass: false,
-      },
-    }),
+    {
+      getSnapshotData: () => ({
+        snapshotPath: "snapshot-path",
+        snapshotIdentifier: "snapshot-identifier",
+        markSnapshotAsReviewed: vitest.fn(),
+        existingSnapshot: {
+          contentHash: getContentHash(sampleElement.outerHTML),
+          failedRules: ["rule-1"],
+          pass: false,
+        },
+      }),
+    },
   );
   const result = await matcher.toPassDesignReview(sampleElement, {
     strict: false,
   });
   expect(result.pass).toBe(true);
+});
+
+test("extendExpectDesignReviewer throws a runtime exception when no cached snapshot is present and we are missing the OpenAI API key", async () => {
+  const sampleElement = document.createElement("div");
+  sampleElement.outerHTML = "<div>sample-element</div>";
+  const matcher = extendExpectDesignReviewer(
+    {
+      customStyles: [],
+      model: { modelName: "gpt-4o-mini" },
+    },
+    {
+      getSnapshotData: () => ({
+        snapshotPath: "snapshot-path",
+        snapshotIdentifier: "snapshot-identifier",
+        markSnapshotAsReviewed: vitest.fn(),
+        existingSnapshot: null,
+      }),
+    },
+  );
+  await expect(matcher.toPassDesignReview(sampleElement)).rejects.toThrow(
+    "No model key provided.",
+  );
+});
+
+test("extendExpectDesignReviewer throws an error when snapshot does not match but we are in CI", async () => {
+  const sampleElement = document.createElement("div");
+  sampleElement.outerHTML = "<div>sample-element</div>";
+
+  const matcher = extendExpectDesignReviewer(
+    {
+      customStyles: [],
+      model: { modelName: "gpt-4o-mini", key: "test-api-key" },
+    },
+    {
+      getTestEnvironment: () => ({ inCI: true }),
+      getSnapshotData: () => ({
+        snapshotPath: "snapshot-path",
+        snapshotIdentifier: "snapshot-identifier",
+        markSnapshotAsReviewed: vitest.fn(),
+        existingSnapshot: null,
+      }),
+    },
+  );
+  await expect(matcher.toPassDesignReview(sampleElement)).rejects.toThrow(
+    "Snapshots must be generated locally.",
+  );
 });
