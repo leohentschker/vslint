f5b77bda282621daf135d31a6ad4d336d2a93937
Leo Hentschker <leowork@Leos-MBP.lan>
2024-12-02 17:48:11 -0500

    Fixing import order

--- packages/server/src/cli.ts
+++ packages/server/src/cli.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
-import { startServer } from "./server";
 import { runDesignEvals } from "./evaluate";
+import { startServer } from "./server";
 
 const args = process.argv.slice(2);
 
